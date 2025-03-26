f099f6367c89cbda07ce68653da75394514c5876
vathsal-s-vikas <shrujanapatil3@gmail.com>
2025-03-26 12:28:30 +0530

    Initial Spring Boot project setup for Google Classroom Clone

---
+++ src/main/java/com/classroom/google_classroom_clone/GoogleClassroomCloneApplication.java
@@ -0,0 +1,13 @@
+package com.classroom.google_classroom_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleClassroomCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleClassroomCloneApplication.class, args);
+	}
+
+}
